2ff02dfe19f2edad2662e84543c951fde5f5e1d7
Andrelino Silva <andrelinodev@gmail.com>
2022-09-19 07:43:01 -0300

    Cors

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,4 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
 import cors from 'cors';
 
 import { PrismaClient } from '@prisma/client';
@@ -9,11 +9,21 @@ const app = express();
 app.use(express.json({ limit: '50mb' }));
 // app.use(express.json());
 // app.use(cors());
-app.use(
-  cors({
-    origin: 'e-sports-andrelinos.vercel.app',
-  }),
-);
+const whiteList = ['https://e-sports-andrelinos.vercel.app/'];
+const corsOptionsDelegate = function (
+  req: any,
+  callback: (arg0: null, arg1: { origin: boolean }) => void,
+) {
+  let corsOptions;
+  if (whiteList.indexOf(req.header('Origin')) !== -1) {
+    corsOptions = { origin: true };
+  } else {
+    corsOptions = { origin: false };
+  }
+  callback(null, corsOptions);
+};
+
+app.use(cors(corsOptionsDelegate));
 
 const prisma = new PrismaClient({
   log: ['query'],
