af301ba0efd729a83c159ba45d20672c0907d25c
Andrelino Silva <andrelinodev@gmail.com>
2022-09-19 07:47:13 -0300

    Cors

--- src/server.ts
+++ src/server.ts
@@ -9,7 +9,12 @@ const app = express();
 app.use(express.json({ limit: '50mb' }));
 // app.use(express.json());
 // app.use(cors());
-const whiteList = ['https://e-sports-andrelinos.vercel.app/'];
+const whiteList = [
+  'https://e-sports-andrelinos.vercel.app/*',
+  '*e-sports-andrelinos.vercel.app/*',
+  '*andrelinos.vercel.app/*',
+  'https://e-sports-andrelinos.vercel.app',
+];
 const corsOptionsDelegate = function (
   req: any,
   callback: (arg0: null, arg1: { origin: boolean }) => void,
